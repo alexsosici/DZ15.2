c062d1106038bbb3ab7fb7eab295a6d8b425ae28
Alex Sosic <alexsosici@yahoo.com>
2020-12-07 17:31:07 +0100

    tekstni fajlovi (web serveri lekcija za dz 16)

--- DZ 15.2..py
+++ DZ 15.2..py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
-
+import datetime
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    some_text = "Message from the handler."
+    current_year = datetime.datetime.now().year
+    return render_template("index.html", some_text=some_text, current_year=current_year)
 @app.route("/about")
 def about():
     return render_template("about.html")
